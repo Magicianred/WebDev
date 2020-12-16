d494f04051301bde079ca4a14671929259807ab0
Kemal Hatunoglu <64790356+Kemalhatunoglu@users.noreply.github.com>
2020-12-16 23:53:03 +0300

    Bitti

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -33,6 +33,12 @@ namespace WebAPI
             services.AddSingleton<IBookService, BookManager>();
             services.AddSingleton<IBookDal, EfBookDal>();
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                    builder => builder.WithOrigins("http://localhost:4200"));
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +48,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
